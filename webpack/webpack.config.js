d06115fb70268400986fbf3cc34d65ac07ef3c1c
Fra <rakouett-du-56@hotmail.fr>
2022-09-05 12:58:14 +0200

    Chrome extension structure

---
+++ webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
+      content: path.join(__dirname, "src/content.ts"),
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   plugins: [
+      new CopyPlugin({
+         patterns: [{from: ".", to: ".", context: "public"}]
+      }),
+   ],
+};
