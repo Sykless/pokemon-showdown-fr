e977e00b55d382f38783fdda13454f518bf529a7
Fra <rakouett-du-56@hotmail.fr>
2023-09-18 16:38:42 +0200

    Fix Serpente-Eau and Vert-De-Fer display in Teambuilder

--- src/teambuilder/teambuilderTranslate.ts
+++ src/teambuilder/teambuilderTranslate.ts
@@ -1815,9 +1815,9 @@ function updateInputPokemonName()
 function convertPokemonNameToArray(pokemonName: string)
 {
 	const HyphenPokemonName = ["nidoran-f", "nidoran-m", "ho-oh", "porygon-z", "jangmo-o", "hakamo-o", "kommo-o", "ama-ama", "tag-tag",
-		"fort-ivoire", "fongus-furie", "pelage-sable", "hurle-queue", "flotte-meche", "rampe-ailes", "rugit-lune"]
+		"fort-ivoire", "fongus-furie", "pelage-sable", "hurle-queue", "flotte-meche", "rampe-ailes", "rugit-lune", "serpente-eau"]
 
-	const TwoHyphenPokemonName = ["roue-de-fer", "mite-de-fer", "paume-de-fer", "tetes-de-fer", "epine-de-fer", "hotte-de-fer", "garde-de-fer"]
+	const TwoHyphenPokemonName = ["roue-de-fer", "mite-de-fer", "paume-de-fer", "tetes-de-fer", "epine-de-fer", "hotte-de-fer", "garde-de-fer", "vert-de-fer"]
 		
 	var pokemonRawName = pokemonName.split("-");
 
